f4c5dc851963e105de1587236cfd2998d64171c2
vinsontandraa <vtandra7@gmail.com>
2021-05-24 11:02:10 +0700

    tambahin mock up

--- src/app/mock-treasures.ts
+++ src/app/mock-treasures.ts
@@ -1,5 +1,24 @@
-export interface Treasure {
-    id: 1,
-    name: 'Windstorm Gold Staff',
-    location: 'Tree behind home',
-};
+// export interface Treasure {
+//     id: 1,
+//     name: 'Windstorm Gold Staff',
+//     location: 'Tree behind home',
+// };
+export interface Treasure  {
+    id: number;
+    name: string;
+    location: string;
+}
+
+export const TREASURES: Treasure[] = [
+    { id: 1, name: 'Windstorm Gold Staff', location: 'Tree behind home' },
+    { id: 2, name: 'Alaric Treasure', location: 'Southern Italy' },
+    { id: 3, name: 'Ganj-e Badavard', location: 'King Khosrow II Tomb' },
+    { id: 4, name: 'Heirloom Seal of the Realm', location: 'Imperial Seal of China' },
+    { id: 5, name: 'Egill Skallagrímssons silver', location: 'Buried near Mosfellsbær, Iceland' },
+    { id: 6, name: 'Kusanagi', location: 'Lost at sea' },
+    { id: 7, name: 'Crown Jewels of England', location: 'lost in The Wash ' },
+    { id: 8, name: 'Llywelyns coronet', location: 'Stolen in Westminster Abbey ' },
+    { id: 9, name: 'La Noche Triste', location: 'Looted from the palace of Moctezuma II' },
+    { id: 10, name: 'Lost Inca gold', location: 'Deep jungle of Inca' }
+];
+
